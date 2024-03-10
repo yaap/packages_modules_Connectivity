940ac9e4db2a15a1e4db5f1bbe8b7383bb00f617
Michael Bestas <mkbestas@gmail.com>
2024-03-11 01:52:21 +0200

    netbpfload: Support <4.14 kernels
    Change-Id: I3d6573c102c1d64a64209bf61ecb8ed0c4ecb64c

--- bpf/loader/NetBpfLoad.cpp
+++ bpf/loader/NetBpfLoad.cpp
@@ -1000,7 +1000,7 @@ static int createMaps(const char* elfPath, ifstream& elfFile, vector<unique_fd>&
               .max_entries = max_entries,
               .map_flags = md[i].map_flags,
             };
-            if (isAtLeastKernelVersion(4, 15, 0))
+            if (isAtLeastKernelVersion(4, 14, 0))
                 strlcpy(req.map_name, mapNames[i].c_str(), sizeof(req.map_name));
 
             bool haveBtf = btf && isBtfSupported(type);
@@ -1239,7 +1239,7 @@ static int loadCodeSections(const char* elfPath, vector<codeSection>& cs, const 
               .kern_version = kvers,
               .expected_attach_type = cs[i].attach_type,
             };
-            if (isAtLeastKernelVersion(4, 15, 0))
+            if (isAtLeastKernelVersion(4, 14, 0))
                 strlcpy(req.prog_name, cs[i].name.c_str(), sizeof(req.prog_name));
             fd.reset(bpf(BPF_PROG_LOAD, req));
 
@@ -1865,12 +1865,14 @@ static int doLoad(char** argv, char * const envp[]) {
         //  kernel does not have CONFIG_BPF_JIT=y)
         // BPF_JIT is required by R VINTF (which means 4.14/4.19/5.4 kernels),
         // but 4.14/4.19 were released with P & Q, and only 5.4 is new in R+.
-        if (writeProcSysFile("/proc/sys/net/core/bpf_jit_enable", "1\n")) return 1;
+        if (writeProcSysFile("/proc/sys/net/core/bpf_jit_enable", "1\n") &&
+            android::bpf::isAtLeastKernelVersion(4, 14, 0)) return 1;
 
         // Enable JIT kallsyms export for privileged users only
         // (Note: this (open) will fail with ENOENT 'No such file or directory' if
         //  kernel does not have CONFIG_HAVE_EBPF_JIT=y)
-        if (writeProcSysFile("/proc/sys/net/core/bpf_jit_kallsyms", "1\n")) return 1;
+        if (writeProcSysFile("/proc/sys/net/core/bpf_jit_kallsyms", "1\n") &&
+            android::bpf::isAtLeastKernelVersion(4, 14, 0)) return 1;
     }
 
     // Create all the pin subdirectories
