c566c90619585183226f258cbc52f7d2568f2e6f
Pierre-Hugues Husson <phh@phh.me>
2025-06-16 16:21:55 -0400

    Some additional handling of bpf-less device
    Change-Id: I7360ef049fd57d0f27a4579719b7ec05bba971b0

--- service/src/com/android/server/BpfNetMaps.java
+++ service/src/com/android/server/BpfNetMaps.java
@@ -914,6 +914,7 @@ public class BpfNetMaps {
             final InetAddress address, final int protocol, final int remotePort,
             final boolean isAllowed) {
         throwIfPre25Q2("addLocalNetAccess is not available on pre-B devices");
+        if (sLocalNetAccessMap == null) return;
         if (iface == null) {
             Log.e(TAG, "Null iface, skip addLocalNetAccess for " + address);
             return;
@@ -951,6 +952,7 @@ public class BpfNetMaps {
     public void removeLocalNetAccess(final int lpmBitlen, @Nullable final String iface,
             final InetAddress address, final int protocol, final int remotePort) {
         throwIfPre25Q2("removeLocalNetAccess is not available on pre-B devices");
+        if (sLocalNetAccessMap == null) return;
         final int ifIndex;
         if (iface == null) {
             ifIndex = 0;
@@ -1066,7 +1068,7 @@ public class BpfNetMaps {
     @RequiresApi(Build.VERSION_CODES.TIRAMISU)
     public int getNetPermForUid(final int uid) {
         final int appId = UserHandle.getAppId(uid);
-        if (sUidPermissionMap == null) return PERMISSION_INTERNET;
+        if (sUidPermissionMap == null) return TRAFFIC_PERMISSION_INTERNET;
         try {
             // Key of uid permission map is appId
             // TODO: Rename map name
