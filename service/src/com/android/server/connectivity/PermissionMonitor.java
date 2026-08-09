lineage-bpf-patch/android_packages_modules_Connectivity

Fix: Prevent NullPointerException in BpfNetMaps during UID removal
This patch adds null checks and exception handling when removing UIDs
from sLocalNetBlockedUidMap in BpfNetMaps and PermissionMonitor.
It prevents potential system_server crashes during boot or app
install/uninstall events on devices where the BPF UID map may be null.

This change improves system stability and avoids random reboots
also tested on custom roms, with old kernels 4.9 4.14

service/src/com/android/server/connectivity/PermissionMonitor.java
@@ -974,10 +974,22 @@ public class PermissionMonitor {
         // Update uid permission.
         updateAppIdTrafficPermission(uid);
         if (BpfNetMaps.isAtLeast25Q2()) {
-            mBpfNetMaps.removeUidFromLocalNetBlockMap(uid);
-            if (hasSdkSandbox(uid)) mBpfNetMaps.removeUidFromLocalNetBlockMap(
-                    sProcessShim.toSdkSandboxUid(uid));
+            try {
+                mBpfNetMaps.removeUidFromLocalNetBlockMap(uid);
+            } catch (NullPointerException e) {
+                Log.w(TAG, "BpfNetMaps.removeUidFromLocalNetBlockMap failed for UID " + uid + ": map is null");
+            }
+
+            try {
+                if (hasSdkSandbox(uid)) {
+                    mBpfNetMaps.removeUidFromLocalNetBlockMap(
+                            sProcessShim.toSdkSandboxUid(uid));
+                }
+            } catch (NullPointerException e) {
+                Log.w(TAG, "BpfNetMaps.removeUidFromLocalNetBlockMap failed for sandbox UID: map is null");
+            }
         }
+
         // Get the appId permission from all users then send the latest permission to netd.
         final int appId = UserHandle.getAppId(uid);
         final int appIdTrafficPerm = getAppIdTrafficPermission(appId);
@@ -989,6 +1001,7 @@ public class PermissionMonitor {
         // mUidToNetworkPerm to check if the package can bypass VPN.
         updateVpnUid(uid, false /* add */);
         updateLockdownUid(uid, false /* add */);
+
         // If the package has been removed from all users on the device, clear it form mAllApps.
         if (mPackageManager.getNameForUid(uid) == null) {
             mAllApps.remove(appId);
@@ -1028,6 +1041,7 @@ public class PermissionMonitor {
             }
         }
     }
+
 
     private static int getNetdPermissionMask(String[] requestedPermissions,
                                              int[] requestedPermissionsFlags) {

yaap/packages_modules_Connectivity

service/src/com/android/server/connectivity/PermissionMonitor.java
@@ -1324,9 +1324,13 @@ public class PermissionMonitor {
         }
 
         if (isAtLeastB() && !mBpfNetMaps.isPermissionPropagationEnabled()) {
-            mBpfNetMaps.removeUidFromLocalNetBlockMap(uid);
-            if (hasSdkSandbox(uid)) mBpfNetMaps.removeUidFromLocalNetBlockMap(
-                    Process.toSdkSandboxUid(uid));
+            try {
+                mBpfNetMaps.removeUidFromLocalNetBlockMap(uid);
+                if (hasSdkSandbox(uid)) mBpfNetMaps.removeUidFromLocalNetBlockMap(
+                        Process.toSdkSandboxUid(uid));
+            } catch (NullPointerException e) {
+                Log.w(TAG, "BpfNetMaps.removeUidFromLocalNetBlockMap failed for UID " + uid + ": map is null");
+            }
         }
 
         // If the newly-removed package falls within some VPN's uid range, update Netd with it.
@@ -1335,6 +1339,7 @@ public class PermissionMonitor {
         // mUidToNetworkPerm to check if the package can bypass VPN.
         updateVpnUid(uid, false /* add */);
         updateLockdownUid(uid, false /* add */);
+
         // If the package has been removed from all users on the device, clear it form mAllApps.
         if (mPackageManager.getNameForUid(uid) == null) {
             mAllApps.remove(appId);
@@ -1373,6 +1378,7 @@ public class PermissionMonitor {
             }
         }
     }
+
 
     private static int getNetdPermissionMask(String[] requestedPermissions,
                                              int[] requestedPermissionsFlags) {